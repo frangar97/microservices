3d3f286bb539e01c8994cca5167dc1fc2d51585c
Francisco <frangarcia38@hotmail.com>
2022-12-24 09:22:45 -0600

    Connecting broker with logger service and fixing mongo environment variables

--- brokerservice/cmd/api/handlers.go
+++ brokerservice/cmd/api/handlers.go
@@ -12,9 +12,15 @@ type AuthPayload struct {
 	Password string `json:"password"`
 }
 
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 func (app *Config) broker(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +44,8 @@ func (app *Config) handleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		authenticate(w, requestPayload.Auth)
+	case "log":
+		logItem(w, requestPayload.Log)
 	default:
 		errorJSON(w, errors.New("unknown action"))
 	}
@@ -85,6 +93,38 @@ func authenticate(w http.ResponseWriter, a AuthPayload) {
 	payload.Error = false
 	payload.Message = "Authenticated"
 	payload.Data = jsonFromService.Data
+
+	writeJSON(w, http.StatusAccepted, payload)
+}
+
+func logItem(w http.ResponseWriter, a LogPayload) {
+	jsonData, _ := json.Marshal(a)
+
+	request, err := http.NewRequest(http.MethodPost, "http://loggerservice/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		errorJSON(w, errors.New("error calling logger service"))
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
 
 	writeJSON(w, http.StatusAccepted, payload)
 }
